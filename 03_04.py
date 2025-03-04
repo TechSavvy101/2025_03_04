5ad3fb85be921794bc639f691df3509289d0c772
Thando <166392873+TechSavvy101@users.noreply.github.com>
2025-03-04 11:48:53 +0200

    quest

--- 03_04.py
+++ 03_04.py
@@ -1,3 +1,5 @@
+#1. Write a function to take user input and display it.
+#Question: Write a Python function named get_and_display_input() that:
 #question 1
 
 #question 2 Kitso
@@ -5,7 +7,8 @@
 #question 3 Kitso
 
 #question 4
-
+def read_from_file():
+    
 #question 5
 
 #question 6 Tshiamo Mafojane
