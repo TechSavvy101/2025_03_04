b98760eaee4639e9fa4e2df7a8568cddb2153b29
Kitso-Helepi <kitsohelepi@gmail.com>
2025-03-04 12:02:35 +0200

    Q2&Q3

--- 03_04.py
+++ 03_04.py
@@ -1,8 +1,33 @@
 #question 1
 
 #question 2 Kitso
+def sum_two_numbers(a, b):
+    return a + b
+
+a = int(input("please enter your first value"))
+b = int(input("please enter your second value"))
+    
+result = sum_two_numbers(a, b)
+print(result)
+
 
 #question 3 Kitso
+def save_to_file():
+
+    user_input = input("Please enter the text you want to save to the file: ")
+    
+
+    with open("user_input.txt", "w") as file:
+        file.write(user_input)
+    
+    
+    print("Your input has been saved to user_input.txt.")
+
+
+save_to_file()
+
+
+
 
 #question 4
 
